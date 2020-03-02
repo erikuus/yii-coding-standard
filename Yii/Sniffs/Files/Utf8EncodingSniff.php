70767ca8017d9db0b2d8313f918f9c58b2d66fbd
Erik Uus <erik.uus@gmail.com>
2020-03-02 16:53:44 +0200

    Reformat

--- Yii/Sniffs/Files/Utf8EncodingSniff.php
+++ Yii/Sniffs/Files/Utf8EncodingSniff.php
@@ -27,57 +27,57 @@
 class Yii_Sniffs_Files_Utf8EncodingSniff implements PHP_CodeSniffer_Sniff
 {
 
-	/**
-	 * A list of tokenizers this sniff supports.
-	 *
-	 * @var array	
-	 */
-	public $supportedTokenizers = array(
-					'PHP',
-	);
+    /**
+     * A list of tokenizers this sniff supports.
+     *
+     * @var array
+     */
+    public $supportedTokenizers = array(
+        'PHP',
+    );
 
-	/**
-	 * Returns the token types that this sniff is interested in.
-	 *
-	 * @return array(int)
-	 */
-	public function register()
-	{
-		return array(T_OPEN_TAG);
-	}//end register()
+    /**
+     * Returns the token types that this sniff is interested in.
+     *
+     * @return array(int)
+     */
+    public function register()
+    {
+        return array(T_OPEN_TAG);
+    }//end register()
 
-	/**
-	 * Processes this test, when one of its tokens is encountered.
-	 *
-	 * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
-	 * @param int                  $stackPtr  The position of the current token
-	 *                                        in the stack passed in $tokens.
-	 *
-	 * @return void
-	 */
-	public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
-	{
-		// chech the first open tag.
-		if ($stackPtr !== 0) {
-			if ($phpcsFile->findPrevious(T_OPEN_TAG, ($stackPtr - 1)) !== false)
-				return;
-		}
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token
+     *                                        in the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        // chech the first open tag.
+        if ($stackPtr !== 0) {
+            if ($phpcsFile->findPrevious(T_OPEN_TAG, ($stackPtr - 1)) !== false) {
+                return;
+            }
+        }
 
-		$filePath    = $phpcsFile->getFilename();
-		$fileContent = file_get_contents($filePath);
-		$encoding    = mb_detect_encoding($fileContent, 'UTF-8, ASCII, ISO-8859-1');
+        $filePath    = $phpcsFile->getFilename();
+        $fileContent = file_get_contents($filePath);
+        $encoding    = mb_detect_encoding($fileContent, 'UTF-8, ASCII, ISO-8859-1');
 
-		if ($encoding !== 'UTF-8') {
-			$fileName = basename($filePath);
-			
-			if ($encoding) {
-				$error = 'File must use only UTF-8 encoding. but %s found';
-				$phpcsFile->addError($error, $stackPtr, 'FileIsNotUTF8Encoded', array($encoding));
-			} else {
-				$error = 'File must use only UTF-8 encoding.';
-                                $phpcsFile->addError($error, 0);
-			}
-		}
-	}//end process()
+        if ($encoding !== 'UTF-8') {
+            $fileName = basename($filePath);
 
+            if ($encoding) {
+                $error = 'File must use only UTF-8 encoding. but %s found';
+                $phpcsFile->addError($error, $stackPtr, 'FileIsNotUTF8Encoded', array($encoding));
+            } else {
+                $error = 'File must use only UTF-8 encoding.';
+                $phpcsFile->addError($error, 0);
+            }
+        }
+    }//end process()
 }//end class
